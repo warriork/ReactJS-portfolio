c1dcee7bb313cde96164d62d5c3c65b5102d8b0d
Ihor Fesina <warriork123456@gmail.com>
2022-09-04 21:43:17 +0200

    fixes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,22 +6,25 @@ const app = express()
 const port = 1234
 
 app.use(cors())
-console.log('works')
 type User = {
   id: string
   name: string
 }
-const formatValue = (inputValue: string) => inputValue.toLowerCase().trim().replace(/ +/g, '').replace(/[y]/g, 'i')
+const formatValue = (inputValue: string) =>
+  inputValue.toLowerCase().trim().replace(/ +/g, '').replace(/[y]/g, 'i')
 
 app.get('/', (req, res, next) => {
-  console.log('někdo volá server')
   try {
     const dataString = fs.readFileSync(`${__dirname}/../data.json`, 'utf-8')
     const data = JSON.parse(dataString).users as User[]
-    res.send(data.filter(item => formatValue(item.name).includes(formatValue(req.query.search!.toString()))))
-} catch (err) {
-  next (err)
- }
+    res.send(
+      data.filter(item =>
+        formatValue(item.name).includes(formatValue(req.query.search!.toString()))
+      )
+    )
+  } catch (err) {
+    next(err)
+  }
 })
 
 app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
@@ -29,9 +32,6 @@ app.use((err: Error, req: express.Request, res: express.Response, next: express.
   res.status(500).json('Something got wrong')
   res.json(err)
 })
-
-
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
--- src/httpFilter/HttpFilterApp.tsx
+++ src/httpFilter/HttpFilterApp.tsx
@@ -1,23 +1,23 @@
-import { Div_Wrapper } from '../components/Div_Wrapper'
+import { Helmet } from 'react-helmet'
 import { filterUrl } from '../urls'
+import { isConstructorDeclaration } from 'typescript'
+import { styles } from '../theme'
 import React, { useState } from 'react'
 import styled from 'styled-components'
 
 type User = { id: string; name: string }
 
 export const HttpFilterApp = () => {
-  const [value, setValue] = useState('')
   const [data, setData] = useState([] as User[])
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(null as null | 'Server side error')
 
   const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value)
     setError(null)
     setIsLoading(true)
     try {
       setIsLoading(true)
-      const response = await fetch(filterUrl(value))
+      const response = await fetch(filterUrl(e.target.value))
       if (!response.ok) throw Error
       setData(await response.json())
     } catch (err) {
@@ -28,25 +28,53 @@ export const HttpFilterApp = () => {
   }
   return (
     <Div_Wrapper>
+      <Helmet>
+        <title>HTTP Filter</title>
+      </Helmet>
+
       <h1>HTTP filter search</h1>
-      <Input_Styled
-        type='text'
-        value={value}
-        onChange={handleChange}
-        placeholder='Enter a name'
-      ></Input_Styled>
+      <Input_Styled type='text' onChange={handleChange} placeholder='Enter a name'></Input_Styled>
       <DataList>
         {isLoading ? (
-          <p>Loading</p>
+          <P_Styled>Loading</P_Styled>
         ) : error ? (
-          <p>{error}</p>
+          <P_Styled>{error}</P_Styled>
+        ) : data.length === 0 ? (
+          <P_Styled>No matches</P_Styled>
         ) : (
-          data.map(item => <p key={item.id}>{item.name}</p>)
+          data.map(item => <P_Styled key={item.id}>{item.name}</P_Styled>)
         )}
       </DataList>
     </Div_Wrapper>
   )
 }
 
-const Input_Styled = styled.input``
-const DataList = styled.div``
+const Input_Styled = styled.input`
+  border: 2px solid ${styles.color.black};
+  border-radius: 5px;
+  width: 200px;
+  height: 30px;
+  margin-top: 20px;
+  font-size: 16px;
+  padding-left: 5px;
+`
+const P_Styled = styled.p`
+  line-height: 1.6rem;
+`
+const DataList = styled.div`
+  width: 200px;
+  padding: 10px;
+`
+const Div_Wrapper = styled.div`
+  padding-top: 30px;
+  width: 100%;
+  min-height: calc(100vh - 80px);
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  background: ${styles.color.main};
+  @media (max-width: ${styles.breakingPoints.s}) {
+    min-height: calc(100vh-40px);
+  }
+`
